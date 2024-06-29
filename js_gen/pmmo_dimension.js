8aab740cb91bdc9fc061c05e685d3f34234c2a9b
panda <pandaismyname1@gmail.com>
2024-06-29 17:16:07 +0300

    Balancing Nether & End for default minecraft entities

--- js_gen/pmmo_dimension.js
+++ js_gen/pmmo_dimension.js
@@ -1,11 +1,57 @@
+const fs = require('fs')
+// CONSTS
+let entity_template = `
+"{mod_name}:{entity_name}": {
+    "minecraft:generic.max_health": {max_health},
+    "minecraft:generic.attack_damage": {attack_damage},
+    "minecraft:generic.armor": {armor},
+}
+`
+let dimension_template = `
+{
+  "vein_blacklist": [],
+  "travel_req": {},
+  "mob_modifier": {
+  {entity_properties}
+  },
+  "override": false,
+  "isTagFor": [],
+  "bonus": {}
+}
+`
 
-generate("blue_skies", "*", 2, "+", 4, "+", 4)
-generate("aether", "*", 4, "+", 10, "+", 10)
-generate("undergarden", "*", 10, "*", 5, "+", 15)
-generate("deeperdarker", "*", 20, "*", 10, "+", 20)
-generate("theabyss", "*", 30, "*", 15, "+", 30)
+// MAIN
+// generate("blue_skies", "*", 2, "+", 4, "+", 4)
+// generate("aether", "*", 4, "+", 10, "+", 10)
+// generate_per_dim("minecraft", "the_nether", "*", 5, "+", 12, "+", 12)
+// generate("undergarden", "*", 10, "*", 5, "+", 15)
+// generate_per_dim("minecraft", "the_end", "*", 15, "+", 30, "+", 18)
+// generate("deeperdarker", "*", 20, "*", 10, "+", 20)
+// generate("theabyss", "*", 30, "*", 15, "+", 30)
 
+
+//FUNCTIONS
 function generate(mod_name, health_modifier_mode, health_modifier_value, damage_modifier_mode, damage_modifier_value, armor_modifier_mode, armor_modifier_value) {
+
+    let generated_datapack_path = "../world/datapacks/generated_pack/data/" + mod_name + "/pmmo"
+    let dimensions_folder_path = generated_datapack_path + "/dimensions"
+
+    let dimension_names = []
+
+    let dimension_files = fs.readdirSync(dimensions_folder_path)
+    for (let i = 0; i < dimension_files.length; i++) {
+        let dimension_file = dimension_files[i]
+        let dimension_name = dimension_file.split(".")[0]
+        dimension_names.push(dimension_name)
+    }
+
+    for (let i = 0; i < dimension_names.length; i++) {
+        let dimension_name = dimension_names[i]
+        generate_per_dim(mod_name, dimension_name, health_modifier_mode, health_modifier_value, damage_modifier_mode, damage_modifier_value, armor_modifier_mode, armor_modifier_value);
+    }
+}
+
+function generate_per_dim(mod_name, dimension_name, health_modifier_mode, health_modifier_value, damage_modifier_mode, damage_modifier_value, armor_modifier_mode, armor_modifier_value) {
     if (health_modifier_mode === '*' && health_modifier_value < 2) {
         console.error("Health modifier value must be greater than 2 when using * mode")
         return;
@@ -19,36 +65,12 @@ function generate(mod_name, health_modifier_mode, health_modifier_value, damage_
         return;
     }
 
-    const fs = require('fs')
 
     let generated_datapack_path = "../world/datapacks/generated_pack/data/" + mod_name + "/pmmo"
     let dimensions_folder_path = generated_datapack_path + "/dimensions"
     let entities_folder_path = generated_datapack_path + "/entities"
 
-    let entity_names = []
-    let dimension_names = []
-
     let entity_files = fs.readdirSync(entities_folder_path)
-    for (let i = 0; i < entity_files.length; i++) {
-        let entity_file = entity_files[i]
-        let entity_name = entity_file.split(".")[0]
-        entity_names.push(entity_name)
-    }
-
-    let dimension_files = fs.readdirSync(dimensions_folder_path)
-    for (let i = 0; i < dimension_files.length; i++) {
-        let dimension_file = dimension_files[i]
-        let dimension_name = dimension_file.split(".")[0]
-        dimension_names.push(dimension_name)
-    }
-
-    let entity_template = `
-"{mod_name}:{entity_name}": {
-    "minecraft:generic.max_health": {max_health},
-    "minecraft:generic.attack_damage": {attack_damage},
-    "minecraft:generic.armor": {armor},
-}
-`
 
     let entity_properties = []
     for (const entityFile of entity_files) {
@@ -59,27 +81,12 @@ function generate(mod_name, health_modifier_mode, health_modifier_value, damage_
         let entityProperties = entity_template.replace("{mod_name}", mod_name).replace("{entity_name}", entityName).replace("{max_health}", maxHealth).replace("{attack_damage}", attackDamage).replace("{armor}", armor)
         entity_properties.push(entityProperties)
     }
-    let entity_properties_string = entity_properties.join(",\n")
+    let entity_properties_string = entity_properties.join(",\n");
 
-    let dimension_template = `
-{
-  "vein_blacklist": [],
-  "travel_req": {},
-  "mob_modifier": {
-  {entity_properties}
-  },
-  "override": false,
-  "isTagFor": [],
-  "bonus": {}
-}
-`
 
-    for (let i = 0; i < dimension_names.length; i++) {
-        let fileName = dimension_names[i]
-        let entityProperties = entity_properties_string
-        let dimensionProperties = dimension_template.replace("{entity_properties}", entityProperties)
-        let output_path = dimensions_folder_path + "/" + fileName
+    let fileName = dimension_name;
+    let dimensionProperties = dimension_template.replace("{entity_properties}", entity_properties_string)
+    let output_path = dimensions_folder_path + "/" + fileName
 
-        fs.writeFileSync(output_path + ".json", dimensionProperties)
-    }
+    fs.writeFileSync(output_path + ".json", dimensionProperties)
 }
